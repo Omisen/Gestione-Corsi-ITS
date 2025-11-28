7f7ca0d26fc6ee3327fa81dc2ac12e8fb916bb13
toast <candyrad25@gmail.com>
2025-11-28 12:33:17 +0100

    Update Esami.py

--- backend/models/Esami.py
+++ backend/models/Esami.py
@@ -1,6 +1,7 @@
 from app import db
+from datetime import datetime
 
 class Esami(db.Document):
-    data = db.DateTimeField(required=True)
-    voto = db.IntField(required=True)
-    note = db.StringField(required=True)
+    data = db.DateTimeField(required=True, default=datetime.now())
+    voto = db.IntField(required=True, min_value = 0, max_value = 30)
+    note = db.StringField(required=False, max_length=200)
