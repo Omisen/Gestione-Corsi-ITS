706194a1b9ac46c9365e8e321e8306e5bdcfbee6
Omisen <simone.conzo@outlook.it>
2025-11-28 11:59:14 +0100

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 
 #region Connection
 app.config['MONGODB_SETTINGS'] ={
-    'db' : 'my-test',
+    'db' : 'gestore-corsi',
     'host' : 'localhost',
     'port' : 27017
 }
@@ -16,7 +16,7 @@ db = MongoEngine(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Server Avviato"
 
 if __name__ == '__main__':
     app.run(debug = True)
